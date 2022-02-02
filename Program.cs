e330cd5b0ba3af990d72670fa6d0f6da76ea3e2f
Hugo Licea <h.leon@elranchoinc.com>
2022-02-02 11:30:37 -0600

    Adding Milest to Kilometers method

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace CItest2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(String.Format("{0:N}", ConvertMilesToKilometers(130000)));
         }
+
+        static double ConvertMilesToKilometers(double miles) => (miles * 1.609);
     }
 }
